367ad931f6ffd644621bf82dd2356a8ee83a31b0
Zhi <zhihuahan72@gmail.com>
2020-04-30 17:25:09 -0600

    added speed limit and better controls

--- Assets/Scenes/NewBehaviourScript.cs
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     public Rigidbody rb;
-
+    public float maxSpeed = 25f;
     public float forwardForce = 2000f;
 
     // Start is called before the first frame update
@@ -17,11 +18,12 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
+        
+        var v = rb.velocity;
 
         if (Input.GetKey("w"))
         {
-            rb.AddForce(0, 0, 75 * Time.deltaTime, ForceMode.VelocityChange);
+            rb.AddForce(0, 25 * Time.deltaTime, 25 * Time.deltaTime, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey("d"))
@@ -36,7 +38,15 @@ public class NewBehaviourScript : MonoBehaviour
 
         if (Input.GetKey("s"))
         {
-            rb.AddForce(0, 0, -75 * Time.deltaTime, ForceMode.VelocityChange);
+            rb.AddForce(0, -25 * Time.deltaTime, -25 * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        var yz_mag = (float)Math.Sqrt(v.y * v.y + v.z * v.z); //Get the magnitude in YZ direction
+        if (yz_mag > maxSpeed)
+        {
+            var v_norm = new Vector3(v.x, v.y / yz_mag * maxSpeed, v.z / yz_mag * maxSpeed); //Create a new vector normalized in YZ
+            rb.velocity = v_norm;
+            Debug.Log("clamp");
         }
     }
 }
