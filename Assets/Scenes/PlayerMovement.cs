262f3f896df52d49f5486417e72461c325a32db3
Zhi <zhihuahan72@gmail.com>
2020-04-30 18:54:59 -0600

    renamed stuff and changed the background

--- Assets/Scenes/PlayerMovement.cs
+++ Assets/Scenes/PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
     public float maxSpeed = 25f;
