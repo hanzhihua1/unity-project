479a4425c7cb6c584e738434cda0f471c6c94279
sjcarmena <armena@ualberta.ca>
2020-05-02 17:33:02 -0600

    Obstacles tagged

--- Assets/Scenes/PlayerDeath.cs
+++ Assets/Scenes/PlayerDeath.cs
@@ -4,7 +4,7 @@ public class PlayerDeath : MonoBehaviour
 {
     void OnCollisionEnter(Collision collisionInfo)
     {
-        if (collisionInfo.collider.name != "Ground")
+        if (collisionInfo.collider.tag == "Obstacle")
             Debug.Log("zhi is a big baby");
     }
 }
